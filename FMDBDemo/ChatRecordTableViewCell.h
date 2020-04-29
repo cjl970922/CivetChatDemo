967efbd5d53782dbfafeae9d0301bf2af52ce6b4
曹佳龙 <1311277784@qq.com>
2020-04-29 11:59:15 +0800

    333

--- FMDBDemo/ChatRecordTableViewCell.h
+++ FMDBDemo/ChatRecordTableViewCell.h
@@ -7,10 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MessageModel.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ChatRecordTableViewCell : UITableViewCell
+
+@property(nonatomic,strong) UILabel *rightLabel;
+
+@property (nonatomic,strong) MessageModel *messageModel;
 
 @end
 
